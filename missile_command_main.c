f71782517422bcb4a52470c9b6bd3ba611f739b6
iWebster28 <iWebster28@users.noreply.github.com>
2020-03-27 20:58:36 -0400

    Create missile_command_main.c

---
+++ missile_command_main.c
@@ -0,0 +1,7 @@
+//ECE243 Final Project
+//Missile Command
+//Written in C for the DE1-SoC FPGA, or CPULATOR emulator.
+//Use keyboard and mouse input to shoot down missiles, avoid being hit.
+//Project Start 3-27-20
+//Project End: 	4-9-20 9PM
+//Eric Ivanov and Ian Webster
