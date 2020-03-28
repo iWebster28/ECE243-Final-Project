5eda6d2e38178f5a28e8472586886b68475c0f8a
iWebster28 <iWebster28@users.noreply.github.com>
2020-03-27 21:00:54 -0400

    Update missile_command_main.c

--- missile_command_main.c
+++ missile_command_main.c
@@ -2,6 +2,6 @@
 //Missile Command
 //Written in C for the DE1-SoC FPGA, or CPULATOR emulator.
 //Use keyboard and mouse input to shoot down missiles, avoid being hit.
-//Project Start 3-27-20
-//Project End: 	4-9-20 9PM
+//Project Start: 3-27-20
+//Project End: 	4-9-20, 9PM
 //Eric Ivanov and Ian Webster
